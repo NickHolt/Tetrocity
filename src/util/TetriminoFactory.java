90f14b5dfa27a05db88196887093f5d35e7681db
Nick Holt <nickholt93@gmail.com>
2013-11-17 18:14:44 -0800

    Added TetriminoFactory.java initial framework.

--- src/util/TetriminoFactory.java
+++ src/util/TetriminoFactory.java
@@ -1,38 +1,105 @@
 package util;
 
 import model.Shape;
+import model.Tetrimino;
 
-/** A pseudo-random generator of Tetrimino piece information for a game of Tetrocity. To 
- * instantiate a Generator, a seed and Tetrimino block-length range must be
- * provided. Note that two Generators of the same seed and block-length range will
+/** A pseudo-random generator of {@link Tetrimino} piece information for a game of Tetrocity. To 
+ * instantiate a TetriminoFactory, a seed and Tetrimino block-length range must be
+ * provided. Note that two Factories of the same seed and block-length range will
  * generate an identical sequence of Tetrimino pieces. 
  * 
- *  A Generator instance will also be able to assign Tetrimino IDs that are
+ *  A TetriminoFactory can provide random {@link Shape} objects, but also random Tetrimino
+ * objects if a root coordinate is provided. 
+ * 
+ *  A TetriminoFactory instance will also be able to assign Tetrimino IDs that are
  * unique to all Tetriminoes currently in the game. As such, when a Tetrimino
- * is deleted entirely, it is necessary to notify the Generator that that ID
- * is now free to use, via {@link ShapeGenerator#freeID()}. 
+ * is deleted entirely, it is necessary to notify the TetriminoFactory that that ID
+ * is now free to use, via {@link TetriminoFactory#freeID()}. A TetriminoFactory
+ * can only generate IDs unique that instantiation. As such, only one generator
+ * should be used for a given game of Tetrocity (indeed you should only need one). 
  * 
  * 
  * @author Nick Holt
  *
  */
 public class TetriminoFactory {
+    public static final int DEFAULT_LOAD = 100;
     
-    /** Returns a pseudo-random Tetrimino Shape for a given LENGTH 
-     * by use of the provided SEED. 
+    private int [] mLengthRange;
+    private int mSeed;
+    private int[] mIDs; //ID available IFF mIDs[ID] == 1
+
+    /** A new TetriminoFactory. 
      * 
-     * @param length The number of Blocks this layout will order.  
-     * @param seed The seed to generate random behavior. 
-     * @return A new, pseudo-randomly constructed Tetrimino. 
+     * @param lengthRange The range of Tetrimino lengths which this TetriminoFactory 
+     * will generate.
+     * @param seed The seed of all random operations of this TetriminoFactory.
+     * @param initialLoad The initial scope of this TetriminoFactory's ID generation.a
      */
-    public static Shape getRandomShape(byte length, int seed) {
+    public TetriminoFactory(int[] lengthRange, int seed, int initialLoad) {
+        //TODO
+    }
+    
+    /** A new TetriminoFactory with initial load DEFAULT_LOAD. 
+     * 
+     * @param lengthRange The range of Tetrimino lengths which this TetriminoFactory 
+     * will generate.
+     * @param seed The seed of all random operations of this TetriminoFactory.
+     */
+    public TetriminoFactory(int[] lengthRange, int seed) {
+        this(lengthRange, seed, DEFAULT_LOAD);
+    }
+    
+    /**
+     * @return A new {@link Shape} instance randomly generated using this
+     * TetriminoFactory's length range and seed. 
+     */
+    public static Shape getRandomShape() {
         //TODO
         return new Shape(null, 0);
     }
     
-    public int getID() {
+    public int getRandomUniqueID() {
         //TODO
         return 0;
+    }
+    
+    public Tetrimino getRandomTetrimino(int[] rootCoordinate) {
+        //TODO
+        return null;
+    }
+    
+    public void freeID(int ID) {
+        //TODO
+    }
+
+    /**
+     * @return The range of Shape lengths used by this TetriminoFactory. 
+     */
+    public int [] getLengthRange() {
+        return mLengthRange;
+    }
+
+    /**
+     * @param mLengthRange The range of Shape lengths to be used by this 
+     * TetriminoFactory.
+     */
+    public void setLengthRange(int [] mLengthRange) {
+        this.mLengthRange = mLengthRange;
+    }
+
+    /**
+     * @return The random seed used by this TetriminoFactory. 
+     */
+    public int getSeed() {
+        return mSeed;
+    }
+
+    /**
+     * @param The random seed to be used by this TetriminoFactory.
+     */
+    public void setSeed(int mSeed) {
+        this.mSeed = mSeed;
     }
 
 }
