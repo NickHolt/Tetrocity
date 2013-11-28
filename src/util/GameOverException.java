fd9bec52c014e9d7e4ce5aacf73f707e4aef3dc3
Nick Holt <nickholt93@gmail.com>
2013-11-28 15:31:45 -0800

    Updated documentation.

--- src/util/GameOverException.java
+++ src/util/GameOverException.java
@@ -1,5 +1,12 @@
 package util;
 
+/** An exception indicated the end of game state has been reached. This state is achieved
+ * when a new Tetrimino cannot be placed in the usual manner without colliding with an already
+ * present Tetrimino.
+ * 
+ * @author Nick Holt
+ *
+ */
 public class GameOverException extends Exception {
     private static final long serialVersionUID = 1L;
 
