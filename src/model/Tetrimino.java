db4a35bbb94481ad613976f15c7d7bddb5ae7b1c
Nick Holt <nickholt93@gmail.com>
2013-11-28 15:25:03 -0800

    Updated documentation.

--- src/model/Tetrimino.java
+++ src/model/Tetrimino.java
@@ -1,17 +1,10 @@
 package model;
 
-import util.Debug;
 import util.Direction;
 
 /** A singular game piece for a game of Tetrocity. A Tetrimino is a 
  * grouping of one or more blocks, with a Shape describing their 
- * relative matrix placements and Tetrimino dimensional information.
- * 
- *  The constituent blocks of a Tetrimino piece do not have classes of their
- * own. Instead, they are represented by matrix-coordinate positions. As such,
- * a Tetrimino is essentially a {@link Shape} with an ID an absolute matrix
- * position. All shape information, such as the relative placement of
- * the Tetrimino's constituent blocks are known by the Tetrimino's Shape.
+ * relative ordering and overall dimensional information.
  * 
  *  In order to provide the absolute matrix position information, a root 
  * coordinate may be provided. A root coordinate is found as such: the row
@@ -20,6 +13,12 @@ import util.Direction;
  * of the top-left corner of the smallest possible box that encapsulates the 
  * entirety of the Tetrimino. 
  * 
+ *  The constituent blocks of a Tetrimino piece do not have classes of their
+ * own. Instead, they are represented by matrix-coordinate positions. As such,
+ * a Tetrimino is essentially a {@link Shape} with an ID an absolute matrix
+ * position. All shape information, such as the relative placement of
+ * the Tetrimino's constituent blocks are known by the Tetrimino's Shape.
+ * 
  *  If a root coordinate is not provided, e.g. the Tetrimino is still in queue, it
  * is critical that it be set before any interaction with the object. 
  * 
@@ -27,15 +26,19 @@ import util.Direction;
  *
  */
 public class Tetrimino {    
+    /* The ID of this Tetrimino, used to determine its color on the GUI. */
     private final int mID;
+    /* True IFF this Tetrimino has been placed in storage previously. */
     private boolean mBeenStored;
+    /* The Shape object describing this Tetrimino's relative block ordering. */
     private final Shape mShape;
+    /* The root coordinate that places this Tetrimino on the Board's grid. */
     private int[] mRootCoordinate;
     
     /** A new Tetrimino whose relative block ordering is described by SHAPE, 
      * and ID is equal to ID. 
      * 
-     *  WARNING: Until the root coordinate is set via {@link Tetrimino#setRootCoordinate(int[])},
+     *  WARNING: Until the root coordinate is set via {@link Tetrimino#setRootCoordinate},
      * all positional methods will throw exceptions. It is critical this is set before any
      * interaction with this Tetrimino.
      * 
@@ -60,8 +63,12 @@ public class Tetrimino {
         mRootCoordinate = rootCoordinate;
     }   
     
+    /****************************/
+    /* Coordinate manipulation. */
+    /****************************/
+    
     /** Shifts the Tetrimimo's coordinate position (i.e. all of its block positions)
-     * one unit in the given DIRECTION. 
+     * one unit in the given Direction. 
      * 
      * @param direction The direction to shift the Tetrimino. 
      */
@@ -74,38 +81,36 @@ public class Tetrimino {
             mRootCoordinate[0] += 1;
         } else if (direction == Direction.WEST) {
             mRootCoordinate[1] -= 1;
-        }
-        
-        Debug.print(2, "Tetrimino " + mID + " shifted " + direction);
+        }        
     }
     
-    /** Deletes the block, if present, located at matrix-coordinate position
+    /** Rotates this Tetrimino piece 90 degrees clockwise.
+     */
+    public void rotateClockwise() {
+        mShape.rotateClockwise();
+    }
+
+    /** Rotates this Tetrimino piece 90 degrees counter-clockwise.
+     */
+    public void rotateCounterClockwise() {
+        mShape.rotateCounterClockwise();
+    }
+
+    /** Deletes the block, if present, located at absolute matrix-coordinate position
      * (row, col) from this Tetrimino. 
      * 
-     * @param row The row coordinate of the block to be deleted. 
-     * @param col The column coordinate of the block to be deleted. 
+     * @param row The absolute row coordinate of the block to be deleted. 
+     * @param col The absolute column coordinate of the block to be deleted. 
      */
     public void deleteBlock(int row, int col) {
         mShape.deleteBlock(row - mRootCoordinate[0], col - mRootCoordinate[1]);        
     }
     
-    /** Rotates this Tetrimino piece 90 degrees clockwise about its rotational 
-     * coordinate.
-     */
-    public void rotateClockwise() {
-        Debug.print(2, "Tetrimino " + mID + "rotated clockwise."); //THIS SLOWS CONTROL DOWN!
-        mShape.rotateClockwise();
-    }
+    /************/
+    /* Getters. */
+    /************/
     
-    /** Rotates this Tetrimino piece 90 degrees counter-clockwise about its rotational 
-     * coordinate.
-     */
-    public void rotateCounterClockwise() {
-        Debug.print(2, "Tetrimino " + mID + "rotated counterclockwise.");
-        mShape.rotateCounterClockwise();
-    }
-    
-    /** Returns a list of [row, column] matrix-coordinates representing this Tetrimino's
+    /** Returns a list of (row, column) absolute  matrix-coordinates representing this Tetrimino's
      * block locations on the matrix rooted on by the root coordinate. 
      * 
      * @return A list of block matrix-coordinates. 
@@ -119,7 +124,6 @@ public class Tetrimino {
                     , relativeCoordinates[i][1] + mRootCoordinate[1]};
         }
         
-        Debug.print(2, "Tetrimino (ID: " + mID + ") coordinates generated.");
         return result;
     }
     
@@ -136,12 +140,12 @@ public class Tetrimino {
     public int getTopRow() {
         return mRootCoordinate[0];
     }
-    
+
     /**
-     * @return True IFF there are no more blocks in this Tetrimino. 
+     * @return True IFF this Tetrimino has ever been stored. 
      */
-    public boolean isEmpty() {
-        return getCoordinates().length == 0;
+    public boolean hasBeenStored() {
+        return mBeenStored;
     }
 
     /**
@@ -166,6 +170,10 @@ public class Tetrimino {
         return mRootCoordinate;
     }
     
+    /************/
+    /* Setters. */
+    /************/
+    
     public void setRootCoordinate(int[] rootCoordinate) {
         mRootCoordinate = rootCoordinate;
     }
@@ -176,13 +184,6 @@ public class Tetrimino {
     
     public void setRootColumn(int column) {
         mRootCoordinate[1] = column;
-    }
-
-    /**
-     * @return True IFF this Tetrimino has ever been stored. 
-     */
-    public boolean hasBeenStored() {
-        return mBeenStored;
     }
 
     /** Mark this Tetrimino as having been stored. 
