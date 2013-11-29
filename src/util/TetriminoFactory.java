ef29934621d2d4b7014383164f1fe7e5fdedf327
Nick Holt <nickholt93@gmail.com>
2013-11-28 16:07:24 -0800

    Updated documentation.

--- src/util/TetriminoFactory.java
+++ src/util/TetriminoFactory.java
@@ -10,10 +10,18 @@ import model.Tetrimino;
 /** A pseudo-random generator of {@link Tetrimino} piece information for a game of Tetrocity. To 
  * instantiate a TetriminoFactory, a seed and Tetrimino block-length range must be
  * provided. Note that two Factories of the same seed and block-length range will
- * generate an identical sequence of Tetrimino pieces. 
+ * generate an identical sequence of Tetrimino pieces.
  * 
- *  A TetriminoFactory can provide pseudorandom {@link Shape} objects, but also 
- *  pseudorandom Tetrimino objects if a root coordinate is provided. 
+ *  This class does not generate all Tetriminoes with equal probability. All blocks are
+ * placed purely at random when generating the Tetriminoes shape, and thus pieces with higher 
+ * rotational symmetry are less likely to be produced, if we consider the rotational variations 
+ * of a Tetrimino shape to be 'equal'. In fact, a piece with full rotational symmetry is four 
+ * times less likely to be generated than a piece with no rotational symmetry. 
+ * 
+ *  This is desired behavior. It is assumed that pieces with full rotational symmetry 
+ * are 'harder' pieces, as they cannot be rotated to fit different shapes. Thus, 'harder'
+ * pieces are generated less frequently. The straight-line piece is an exception to this rule,
+ * and the probabilistic issues of their generation is addressed further below. 
  * 
  *  A TetriminoFactory instance will also be able to assign Tetrimino IDs that are
  * unique to all Tetriminoes currently in the game. As such, when a Tetrimino
@@ -22,23 +30,31 @@ import model.Tetrimino;
  * can only generate IDs unique that instantiation. As such, only one generator
  * should be used for a given game of Tetrocity (indeed you should only need one). 
  * 
+ *  TetriminoFactories allow for a probabilistic model of straight-line generation. If 
+ * desired, an expected spacing S may be provided. This means that during random
+ * Tetrimino (not Shape) generation, a maximum-length straight line piece will be generated
+ * with probability 1 / S. In order to address high variance, a max-length straight line
+ * Tetrimino will be generated with probability 1 if a maximum-length straight line piece
+ * was not chosen for the last {@link TetriminoFactory#STRAIGHT_LINE_SPACING_VARIANCE} * S
+ * piece generations. 
  * 
  * @author Nick Holt
  *
  */
 public class TetriminoFactory {
+    /* The minimum variance factor in maximum-length straight line Tetrimino generation. */
+    private static final float STRAIGHT_LINE_SPACING_VARIANCE = 1.25f;
+    /* The range of lengths of generated Tetriminoes. */
     private int [] mLengthRange;
+    /* This TetriminoFactory'r random number generator. */
     private Random mRandom;
+    /* The expected number of Tetrimino generations that occur before it is determined that
+     * a maximum-length Tetrimino piece should be produced. */
     private int mStraightLineSpacing;
+    /* The number of Tetrimino generations that have occured since the last decision to 
+     * produce a maximum-length Tetrimino piece. */
     private int mLastStraightLinePieceProduced;
-    
-    /* TetriminoFactories use length 32 bit-strings to maintain instance unique IDs.
-     * In short, if the ith bit (little-endian)  is 1, then i is an available
-     * ID. If a new ID is requested and all bit-strings are 0, a new bit-string
-     * is added. The jth bit of the ith bit-string is ID 32*i + j. 
-     * Integers are used instead of Longs since Java bitwise operations
-     * are messy on Long types. 
-     */
+    /* The array used to determine available IDs. */
     private ArrayList<Integer> mIDs; 
 
     /** A new TetriminoFactory. This TetriminoFactory will not consider straight-line
@@ -66,25 +82,57 @@ public class TetriminoFactory {
         mRandom = new Random(seed);
         mIDs = new ArrayList<Integer>();
         mStraightLineSpacing = straightLineSpacing;
-        mLastStraightLinePieceProduced = 0;
-        
-        Debug.print(1, "New TetriminoFactory instantiated.");
+        mLastStraightLinePieceProduced = 0;        
     }
     
+    /************/
+    /* Getters. */
+    /************/
+    
+    /** Returns a instance unique ID. These IDs are neither random nor 
+     * non-predictable. 
+     * 
+     *  TetriminoFactories use length 32 bit-strings to maintain instance unique IDs.
+     * In short, if the ith bit (little-endian)  is 1, then i is an available
+     * ID. If a new ID is requested and all bit-strings are 0, a new bit-string
+     * is added. The jth bit of the ith bit-string is ID 32*i + j. 
+     * 
+     * @return A new instance unique ID. 
+     */
+    public int getUniqueID() {
+        int mask, bitString;
+        for (int i = 0; i < mIDs.size(); i++) {
+            mask = 0x1;
+            bitString = mIDs.get(i);
+            
+            if (bitString != 0) { //There is an available ID in the bit-string
+                for (int j = 0; j < 32; j++) {
+                    if ((bitString & mask) != 0) {
+                        mIDs.set(i, bitString & (mask ^ -1)); //zero the bit: ID is used
+                        return 32 * i + j; 
+                    }
+                    mask <<= 1;
+                }
+            }
+        }
+        /* No ID's available, make new bit-string */
+        mIDs.add(0xFFFFFFFE); //0b111...110, since we're going to use first bit for the ID
+        
+        return (mIDs.size() - 1)* 32; //The ID we just said we were going to use
+    }
+
     /** A new random shape matrix that describes a valid Tetrimino shape,
      * for a length randomly selected from this TetriminoFactory's length
      * range. 
      * 
-     * WARNING: The matrix generated by this method does NOT fit the 
+     *  WARNING: The matrix generated by this method does NOT fit the 
      * "smallest possible matrix" representation that Shape objects use.
      * Shape objects automatically shrink input matrices, so it is not done
-     * here in order to save an extra call to Matrices#shrink(). 
-     * 
-     *  That being said, the matrix will be a valid Tetrimino shape description. 
+     * here in order to save an extra call to {@link Matrices#shrink()}. 
      * 
      * @return The random shape matrix. 
      */
-    public int[][] getRandomShapeMatrix() {
+    private int[][] getRandomShapeMatrix() {
         /* This method works by starting with a blank 2L + 1 by 2L + 1 matrix, where L 
          * is the randomly selected Tetrimino length. 2L is used to ensure even a straight
          * line piece will fit, and the + 1 provides necessary buffering. 
@@ -99,7 +147,7 @@ public class TetriminoFactory {
          * 
          */
         int[] lengths = new int[mLengthRange[1] - mLengthRange[0] + 1]; //I know this is dumb, it fixes a
-        for (int i = 0; i < lengths.length; i++) {                      //bug with Java Random.nextInt(int)
+        for (int i = 0; i < lengths.length; i++) {                      //bug with Java Random#nextInt
             lengths[i] = i + mLengthRange[0];
         }
         int random = mRandom.nextInt();
@@ -165,78 +213,15 @@ public class TetriminoFactory {
             blocksLeft--;
         }
         
-        Debug.print(2, "New random shape matrix generated.");
         return matrix;
     }
     
     /**
      * @return A new {@link Shape} instance randomly generated using this
      * TetriminoFactory's length range and seed.
-     * 
-     *  Note that different rotational orientations of the same shape may
-     * be generated by this method. However, since each shape has the 
-     * same number of rotational orientations (4), the distribution
-     * of different shapes will still be uniform. 
      */
-    public Shape getRandomShape() {
+    private Shape getRandomShape() {
         return new Shape(getRandomShapeMatrix());
-    }
-    
-    /** Returns a instance unique ID. These IDs are neither random nor 
-     * non-predictable. 
-     * 
-     * @return A new instance unique ID. 
-     */
-    public int getUniqueID() {
-        int mask, bitString;
-        for (int i = 0; i < mIDs.size(); i++) {
-            mask = 0x1;
-            bitString = mIDs.get(i);
-            
-            if (bitString != 0) { //There is an available ID in the bit-string
-                for (int j = 0; j < 32; j++) {
-                    if ((bitString & mask) != 0) {
-                        mIDs.set(i, bitString & (mask ^ -1)); //zero the bit: ID is used
-                        return 32 * i + j; 
-                    }
-                    mask <<= 1;
-                }
-            }
-        }
-        /* No ID's available, make new bit-string */
-        mIDs.add(0xFFFFFFFE); //0b111...110, since we're going to use first bit for the ID
-        
-        Debug.print(2, "New ID requested.");
-        return (mIDs.size() - 1)* 32; //The ID we just said we were going to use
-    }
-    
-    /** Returns a Tetrimino with a pseudorandom shape and instance unique ID. If 
-     * the straight-line Tetrimino spacing was specified, this method will first ensure
-     * that a straight-line piece was returned in the last straight_line_spacing * 1.25
-     * Tetrimino productions. If not, a straight line piece will be returned with 100% certainty.
-     * This functionality addresses the large standard deviation observed when the straight-line
-     * production is left to probability alone. 
-     * 
-     *  If so, a straight line piece will be produced with probability 1 / straight_line_spacing.
-     * Otherwise, a random Tetrimino piece (including a potentially straight-line Tetrimino) will
-     * be produced.
-     * 
-     * @return The new Tetrimino object. 
-     */
-    public Tetrimino getRandomTetrimino() {
-        Debug.print(1, "New Tetrimino requeted from Factory.");
-        
-        if (mStraightLineSpacing > 0) {
-            if (mLastStraightLinePieceProduced >= mStraightLineSpacing * 1.25
-                    || mRandom.nextInt(mStraightLineSpacing) == 0) {
-                mLastStraightLinePieceProduced = 0;
-                return getRandomMaxLengthStraightLineTetrimino();
-            }
-        }
-        
-        mLastStraightLinePieceProduced++;
-        
-        return new Tetrimino(getRandomShape(), getUniqueID()); 
     }
     
     /**
@@ -257,7 +242,46 @@ public class TetriminoFactory {
         
         return straightLineTetrimino;
     }
+
+    /** Returns a Tetrimino with a pseudorandom shape and instance unique ID. If 
+     * the straight-line Tetrimino spacing was specified, this method will first ensure
+     * that a straight-line piece was returned in the last straight_line_spacing * 
+     * {@link TetriminoFactory#STRAIGHT_LINE_SPACING_VARIANCE} Tetrimino productions. 
+     * If not, a straight line piece will be returned with 100% certainty. This functionality 
+     * addresses the large standard deviation observed when the straight-line production is 
+     * left to probability alone. 
+     * 
+     *  Furthermore, a straight line piece will be produced with probability 1 / 
+     * straight_line_spacing. Otherwise, a random Tetrimino piece (potentially a 
+     * straight-line Tetrimino) will be produced.
+     * 
+     * @return The new Tetrimino object. 
+     */
+    public Tetrimino getRandomTetrimino() {        
+        if (mStraightLineSpacing > 0) {
+            if (mLastStraightLinePieceProduced >= mStraightLineSpacing * STRAIGHT_LINE_SPACING_VARIANCE
+                    || mRandom.nextInt(mStraightLineSpacing) == 0) {
+                mLastStraightLinePieceProduced = 0;
+                return getRandomMaxLengthStraightLineTetrimino();
+            }
+        }
+        
+        mLastStraightLinePieceProduced++;
+        
+        return new Tetrimino(getRandomShape(), getUniqueID()); 
+    }
     
+    /**
+     * @return The range of Shape lengths used by this TetriminoFactory. 
+     */
+    public int[] getLengthRange() {
+        return mLengthRange;
+    }
+    
+    /***********/
+    /* Setters */
+    /***********/
+
     /** Marks an ID as free to use, so that it may be assigned to future 
      * Tetriminoes.
      * 
@@ -282,16 +306,11 @@ public class TetriminoFactory {
         }
         
         mIDs.set(ID / 32,  //ID / 32 is the index of the bit-string that contains the ID
-                mIDs.get(ID / 32) | (0x1 << (ID % 32))); //Set the relevant ID bit to 1
-        
-        Debug.print(2, "ID " + ID + " freed.");
+                mIDs.get(ID / 32) | (0x1 << (ID % 32))); //Set the relevant ID bit to 1        
     }
 
-    /**
-     * @return The range of Shape lengths used by this TetriminoFactory. 
-     */
-    public int[] getLengthRange() {
-        return mLengthRange;
+    public void setStraightLineSpacing(int straightLineSpacing) {
+        mStraightLineSpacing = straightLineSpacing;
     }
 
     /**
@@ -302,9 +321,9 @@ public class TetriminoFactory {
         mLengthRange = lengthRange;
     }
     
-    public void setStraightLineSpacing(int straightLineSpacing) {
-        mStraightLineSpacing = straightLineSpacing;
-    }
+    /**********************************/
+    /* Miscellaneous utility methods. */
+    /**********************************/
     
     /** Checks if an ArrayList of coordinates contains a given coordinate. Coordinates
      * are considered equal if their individual elements are equal. 
@@ -315,6 +334,7 @@ public class TetriminoFactory {
      */
     private static boolean containsCoordinate(ArrayList<int[]> coordinateSet
             , int[] targetCoordinate) {
+        
         for (int[] coordinate : coordinateSet) {
             if (Arrays.equals(coordinate, targetCoordinate)) {
                 return true;
